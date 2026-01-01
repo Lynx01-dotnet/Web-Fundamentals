671fdf0f8b5cb54c4a9277775ef92799856cf02b
Lynx <kp125808@gmail.com>
2026-01-01 21:55:40 +0530

    InitialCurrencyConverter

---
+++ Currency-Converter/app.js
@@ -0,0 +1,57 @@
+const BASE_URL = `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies`
+const dropdowns = document.querySelectorAll(".dropdown select");
+const btn = document.querySelector("button"); 
+const fromCurr = document.querySelector(".from select");
+const toCurr = document.querySelector(".to select");
+const msg = document.querySelector("#msg");
+
+window.addEventListener("load", () => {
+    updateExchangeRate();
+})
+
+for(let select of dropdowns){
+    for (code in countryList){
+        let newOption = document.createElement("option");
+        newOption.textContent = code;
+        newOption.value = code;
+        if (select.name === "from" && code === "USD"){
+            newOption.selected = 'selected';
+        }
+        else if (select.name === "to" && code === "INR"){
+            newOption.selected = 'selected';
+        }
+        select.append(newOption); 
+    }
+    select.addEventListener("change", (evt) => {
+        updateFlag(evt.target);
+    });
+}
+
+const updateFlag = (element) => {
+    let code = element.value;
+    let countryCode = countryList[code]
+    let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`
+    let img = element.parentElement.querySelector('img');
+    img.src = newSrc;   
+}
+
+btn.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    updateExchangeRate();
+})
+
+const updateExchangeRate = async () => {
+    let amount = document.querySelector('.amount input');
+    let amtVal = amount.value;
+    if(amtVal === "" || amtVal < 1) {
+        amtVal = 1;
+        amount.value = 1;
+    }
+    console.log(fromCurr);
+    const URL= `${BASE_URL}/${fromCurr.value.toLowerCase()}.json`;
+    let response = await fetch(URL);
+    let data =await response.json();
+    rate = data[fromCurr.value.toLowerCase()][toCurr.value.toLowerCase()];
+    let finalAmount = amtVal*rate;
+    msg.textContent = `${amtVal} ${fromCurr.value} = ${finalAmount} ${toCurr.value}`;
+}
